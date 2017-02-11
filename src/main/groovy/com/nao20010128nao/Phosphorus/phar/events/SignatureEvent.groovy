46b613fa07ed5f1725310e6eed7a157eec4be2b7
nao20010128nao <nao20010128@gmail.com>
2017-02-11 13:48:04 +0900

    SHA-256とSHA-512に一応対応

--- src/main/groovy/com/nao20010128nao/Phosphorus/phar/events/SignatureEvent.groovy
+++ src/main/groovy/com/nao20010128nao/Phosphorus/phar/events/SignatureEvent.groovy
@@ -31,6 +31,16 @@ class SignatureEvent implements PharParserEvent{
     }
 
     @Memoized
+    boolean isSha256(){
+        (sigFlags&0x0004)!=0
+    }
+
+    @Memoized
+    boolean isSha512(){
+        (sigFlags&0x0008)!=0
+    }
+
+    @Memoized
     boolean isValidMd5(){
         md5&hash.length==16
     }
@@ -41,9 +51,21 @@ class SignatureEvent implements PharParserEvent{
     }
 
     @Memoized
+    boolean isValidSha256(){
+        sha256&hash.length==32
+    }
+
+    @Memoized
+    boolean isValidSha512(){
+        sha512&hash.length==64
+    }
+
+    @Memoized
     String getHashNameFromFlag(){
         if(md5)"md5"
         else if(sha1)"sha-1"
+        else if(sha256)"sha-256"
+        else if(sha512)"sha-512"
         else null
     }
 
